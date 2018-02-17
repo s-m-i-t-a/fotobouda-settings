be7f803503436acc3cc3c22022f1943229611154
Jindrich K. Smitka <jsmitka@smita.info>
2018-02-17 23:04:50 +0100

    Removed example test

--- test/settings_test.exs
+++ test/settings_test.exs
@@ -2,7 +2,7 @@ defmodule SettingsTest do
   use ExUnit.Case
   doctest Settings
 
-  test "greets the world" do
-    assert Settings.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Settings.hello() == :world
+  # end
 end
