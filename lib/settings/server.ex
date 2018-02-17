7497a753cefd2b50fce1434e5afd98ffafd8c899
Ondrej Tucek <ondrej.tucek@gmail.com>
2018-02-17 13:24:11 +0100

    Refactored Model

--- lib/settings/server.ex
+++ lib/settings/server.ex
@@ -4,30 +4,30 @@ defmodule Settings.Server do
   """
   use GenServer
 
-  alias Settings.Settings
+  alias Settings.Model
 
   require Logger
 
   # Server
   #
   @doc false
-  def start_link(settings \\ %Settings{}) do
-    Logger.info(fn -> "Settings server started..." end)
-    GenServer.start_link(__MODULE__, settings, name: __MODULE__)
+  def start_link(model \\ %Model{}) do
+    Logger.info(fn -> "Model server started..." end)
+    GenServer.start_link(__MODULE__, model, name: __MODULE__)
   end
 
   @impl true
-  def init(%Settings{} = state) do
+  def init(%Model{} = state) do
     {:ok, state}
   end
 
   @impl true
-  def handle_call({:get}, _from, %Settings{} = state) do
+  def handle_call({:get}, _from, %Model{} = state) do
     {:reply, state, state}
   end
 
   @impl true
-  def handle_cast({:set, %Settings{} = new_state}, _state) do
+  def handle_cast({:set, %Model{} = new_state}, _state) do
     {:noreply, new_state}
   end
 end
