43eb123b1a0521e1cfd42ef2218d7d287318aca1
Ondrej Tucek <ondrej.tucek@gmail.com>
2018-06-04 17:51:34 +0200

    Refactoring QrCode to own module

--- lib/settings/qr_code.ex
+++ lib/settings/qr_code.ex
@@ -17,7 +17,7 @@ defmodule Settings.QrCode do
             on_photo: false,
             position: :left_down
 
-  def put_qr_code_on_client(model, is_toggled?) do
+  def put_on_client(model, is_toggled?) do
     updated_qr_code =
       model
       |> update_qr_code(:on_client, is_toggled?)
@@ -25,7 +25,7 @@ defmodule Settings.QrCode do
     %{model | qr_code: updated_qr_code}
   end
 
-  def put_qr_code_on_photo(model, is_toggled?) do
+  def put_on_photo(model, is_toggled?) do
     updated_qr_code =
       model
       |> update_qr_code(:on_photo, is_toggled?)
@@ -33,7 +33,7 @@ defmodule Settings.QrCode do
     %{model | qr_code: updated_qr_code}
   end
 
-  def put_qr_code_position(model, new_position)
+  def put_position(model, new_position)
     when new_position in @positions do
     updated_qr_code =
       model
@@ -42,19 +42,19 @@ defmodule Settings.QrCode do
     %{model | qr_code: updated_qr_code}
   end
 
-  def put_qr_code_position_as_string(model) do
-    position =
+  def put_position_as_string(model) do
+    pos =
       model
       |> Map.get(:qr_code)
       |> Map.get(:position)
 
-    put_qr_code_position(model, qr_code_position(position))
+    put_position(model, position(pos))
   end
 
-  defp qr_code_position("left_down"), do: :left_down
-  defp qr_code_position("left_up"), do: :left_up
-  defp qr_code_position("right_down"), do: :right_down
-  defp qr_code_position("right_up"), do: :right_up
+  defp position("left_down"), do: :left_down
+  defp position("left_up"), do: :left_up
+  defp position("right_down"), do: :right_down
+  defp position("right_up"), do: :right_up
 
   defp update_qr_code(model, key, value) do
     {_, update_model} =
